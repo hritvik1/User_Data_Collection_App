b0bee843631a17414d75b133ae3cf23ed1357406
hritvik1 <hritvikmaini00@gmail.com>
2021-04-29 02:46:58 +0530

    added_delete_prompt

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,3 +5,12 @@ function removeAddBtn() {
 function removeShowBtn() {
   document.getElementById('show').style.display = 'none'
 }
+
+function confirmDelete() {
+  const confirmDel = confirm("Are You Sure You Want To Delete This User!!!!");
+  if (confirmDel === true) {
+    return true
+  } else {
+    return false
+  }
+}
