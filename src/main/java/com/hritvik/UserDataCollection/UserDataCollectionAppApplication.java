1709110476b1931f0de13a1d4b4dccb85d5c66b5
hritvik1 <hritvikmaini00@gmail.com>
2021-04-28 21:36:32 +0530

    added_project_base_files

---
+++ src/main/java/com/hritvik/UserDataCollection/UserDataCollectionAppApplication.java
@@ -0,0 +1,13 @@
+package com.hritvik.UserDataCollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataCollectionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataCollectionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hritvik/UserDataCollection/UserDataCollectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hritvik.UserDataCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataCollectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
