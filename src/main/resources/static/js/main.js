62d8bb4a596d4f8975f0b715289bb7d62f633905
hritvik1 <hritvikmaini00@gmail.com>
2021-04-29 08:19:04 +0530

    added_form_validations

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -14,3 +14,56 @@ function confirmDelete() {
     return false
   }
 }
+
+function validate() {
+  const name          = document.getElementById('userName').value,
+        age           = document.getElementById('age').value,
+        email         = document.getElementById('email').value,
+        number        = document.getElementById('mobileNumber').value,
+        address       = document.getElementById('address').value,
+        error_message = document.getElementById("error_message")
+
+  let text,
+      err_symbol = "<i class=\"fas fa-exclamation-triangle\"></i>&nbsp;",
+      email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if(!name && !age && !email && !number){
+    text = "Error! Empty Form.."
+    error_message.innerHTML = err_symbol + text;
+    return false;
+  }
+  if(name.length < 4 || name.length > 15){
+    text = "Name should be between 4 to 15 characters in length";
+    error_message.innerHTML = err_symbol + text;
+    return false;
+  }
+  if(isNaN(age) || age.length < 1 || age.length > 2){
+    text = "Enter a valid age";
+    error_message.innerHTML = err_symbol + text;
+    return false;
+  }
+  if(!email_regex.test(email)) {
+    text = "Enter a valid email address";
+    error_message.innerHTML = err_symbol + text;
+    return false;
+  }
+  if(isNaN(number) || number.length != 10){
+    text = "Enter a valid 10 digit phone number";
+    error_message.innerHTML = err_symbol + text;
+    return false;
+  }
+  if(address.length > 20){
+    text = "Address should be less then 30 characters in length";
+    error_message.innerHTML = err_symbol + text;
+    return false;
+  }
+  error_message.innerHTML = '';
+  return true;
+}
+
+function removeErrMsg() {
+  if(validate()) {
+    document.getElementById("error_message").innerHTML = '';
+    return true
+  }
+}
